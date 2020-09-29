a0ef8cd5aaf1f1875d54b0dd378b476a1b083e90
Mayur <mayurzope555@gmail.com>
2020-09-29 17:39:15 +0530

    [Mayur]ADDED: Use Case 1 Mandatory UserName in the  Email Validation

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,12 +1,29 @@
 /* Welcome to the Email Validation Problem */
 
-import java.util.Scanner;
+import java.util.*;
 
 public class EmailValidation {
 
+	static final Scanner sc = new Scanner(System.in);
+	
+	public static void ValidatePatternUC1(String patternValidate) {
+		
+		boolean matcher = patternValidate.matches("[A-Za-z0-9]+");
+		
+		if(matcher == true) {
+			System.out.println(patternValidate+" Matches the Email Format");
+		}else {
+			System.out.println(patternValidate+" does not Match the Email Format");
+		}
+	} 
+
 	public static void main(String[] args) {
-
+		
 		System.out.println("Welcome to the Email Validation Problem");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the email pattern to be validated");
+		String patternValidate = sc.nextLine();
+		ValidatePatternUC1(patternValidate);
+		
 	}
 }
