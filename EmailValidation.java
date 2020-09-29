44e39cd00a37d976ab3cc51417527e8cb4562885
Mayur <mayurzope555@gmail.com>
2020-09-29 17:43:07 +0530

    [Mayur]ADDED: Use Case 2 Mandatory @Domain in the  Email Validation

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,15 +1,14 @@
 /* Welcome to the Email Validation Problem */
-
 import java.util.*;
 
 public class EmailValidation {
-
-	static final Scanner sc = new Scanner(System.in);
 	
+	static final Scanner sc = new Scanner(System.in);
+
 	public static void ValidatePatternUC1(String patternValidate) {
-		
+
 		boolean matcher = patternValidate.matches("[A-Za-z0-9]+");
-		
+
 		if(matcher == true) {
 			System.out.println(patternValidate+" Matches the Email Format");
 		}else {
@@ -17,13 +16,24 @@ public class EmailValidation {
 		}
 	} 
 
+	public static void ValidatePatternUC2(String patternValidate) {
+
+		boolean matcher = patternValidate.matches("@[A-Za-z]+");
+
+		if(matcher == true) {
+			System.out.println(patternValidate+" matches the Email Format");
+		}else {
+			System.out.println(patternValidate+" does not match.Invalid Format");
+		}
+	} 
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to the Email Validation Problem");
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the email pattern to be validated");
 		String patternValidate = sc.nextLine();
-		ValidatePatternUC1(patternValidate);
-		
+		ValidatePatternUC2(patternValidate);
+
 	}
 }
